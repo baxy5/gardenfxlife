9d7d4f95eb70c7ef32c086f9c3b48a9d905b6523
baxy5 <bakos.soda@gmail.com>
2022-08-03 17:44:31 +0200

    home page design

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,50 @@ export default function Home() {
           </p>
         </div>
       </div>
+
+      <div className="grid grid-cols-none md:grid-cols-2 gap-0 text-center text-white lg:px-40 lg:py-10 m-2">
+        <div className="bg-[#121212] text-start py-10 px-10 lg:py-40 lg:px-20">
+          <h1 className="text-4xl md:text-5xl">Tanfolyam</h1>
+          <p className="text-lg md:text-lg mt-10">
+            Ha érdekel a tanfolyam akkor úgy lehet fizetni hogy a
+            <b> PayPal címemre </b>
+            külditek az összeget:
+            <br />
+            <b>
+              desingglow105@gmail.com <br /> Ár: 75$
+            </b>
+            <br />
+            Ha elküldet, discordon küld el nekem az utalást! <b>sqcsaba#3521</b>
+          </p>
+        </div>
+        <div className="bg-black flex justify-center items-center p-10 h-96 md:min-h-full">
+          <h1 className="text-5xl lg:text-8xl">
+            Tan<span className="text-[#3872C3]">folyam</span>
+          </h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-none md:grid-cols-2 gap-0 text-center text-white lg:px-40 lg:py-10 m-2">
+        <div className="bg-black flex justify-center items-center p-10 h-96 md:min-h-full">
+          <h1 className="text-5xl lg:text-8xl">
+            V.I.P<span className="text-[#3872C3]"> Signal</span>
+          </h1>
+        </div>
+        <div className="bg-[#121212] text-start py-10 px-10 lg:py-40 lg:px-20">
+          <h1 className="text-4xl md:text-5xl">Tanfolyam</h1>
+          <p className="text-lg md:text-lg mt-10">
+            Ha érdekel a <b>Telegram V.I.P Csoport</b> akkor úgy lehet fizetni
+            hogy a<b> PayPal címemre </b>
+            külditek az összeget:
+            <br />
+            <b>
+              desingglow105@gmail.com <br /> Ár: 75$/hó
+            </b>
+            <br />
+            Ha elküldet, discordon küld el nekem az utalást! <b>sqcsaba#3521</b>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
